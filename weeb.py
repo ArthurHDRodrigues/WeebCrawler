6eb65646d45137e0607b483de6c5cb189e9cc7d6
ArthurHDRodrigues <arthur@macna.ime.usp.br>
2019-08-02 14:17:20 -0300

    commit inicial

---
+++ weeb.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import sys
+import requests
+
+page = requests.get(sys.argv[1], timeout=50)
+
+page_content = BeautifulSoup(page.content, "html.parser")
+
+print(page_content)
+#print(page_content.find_all('a href'))
